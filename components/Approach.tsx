3cf4e9fe346c4c62947b341dcca1d04183e91f32
pranavi2 <pranavirao14@gmail.com>
2024-09-04 19:01:26 +0530

    Updated portfolio content and layout

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -13,11 +13,9 @@ const Approach = () => {
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         {/* add des prop */}
         <Card
-          title="Planning & Strategy"
+          title="Learning & Exploration"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des="I dive into the basics, explore new technologies, and figure out which areas I want to focus on more."
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -26,11 +24,10 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Development & Progress Update"
+          title="Skill Development & Projects"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des="I take what I’ve learned and build real projects to level up my skills and make my portfolio stand out.
+"
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -48,11 +45,10 @@ const Approach = () => {
           {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
         </Card>
         <Card
-          title="Development & Launch"
+          title="Specialization & Career Preparation"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          des="I hone in on a specific area, refine my portfolio, and get ready for internships and job applications.
+"
         >
           <CanvasRevealEffect
             animationSpeed={3}

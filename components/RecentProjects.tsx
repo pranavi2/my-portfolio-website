0bec41a1c453e7daa73f9b6b3f6ba7b2b1d85e26
Pranavi <116634403+pranavi2@users.noreply.github.com>
2025-04-16 23:46:13 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,9 +18,11 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-            <PinContainer
-              title="Projects"
+            
+           <PinContainer
+              title="/ui.aceternity.com"
+              href="https://twitter.com/mannupaaji"
+            >
               
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
@@ -74,7 +76,7 @@ const RecentProjects = () => {
                 </div>
               </div>
             </PinContainer>
-              </a>
+              
           </div>
         ))}
       </div>
